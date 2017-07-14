dac955b57f83b75166a856787eefbc781e29e706
mscherer <mark.scherer@spryker.com>
2017-07-14 11:21:12 +0200

    Add test case.

--- tests/test_files/Controller/Component/MyOtherComponent.php
+++ tests/test_files/Controller/Component/MyOtherComponent.php
@@ -5,6 +5,7 @@ use Cake\Controller\Component;
 
 /**
  * @property \App\Controller\Component\RequestHandlerComponent $RequestHandler
+ * @property \Cake\Controller\Component\SecurityComponent Security
  * @property \Cake\Controller\Component\FlashComponent $Flash
  */
 class MyOtherComponent extends Component {
@@ -12,6 +13,6 @@ class MyOtherComponent extends Component {
 	/**
 	 * @var array
 	 */
-	public $components = ['Flash', 'RequestHandler', 'SomeInvalidOneWillBeIgnored'];
+	public $components = ['Flash', 'RequestHandler', 'SomeInvalidOneWillBeIgnored', 'Security'];
 
 }
