890a760ff92d7d8db023f11068bb644ba23d3c62
dereuromark <euromark@web.de>
2017-09-26 01:11:12 +0200

    Add associations to autocomplete.

---
+++ src/Generator/Task/TableAssociationTask.php
@@ -0,0 +1,46 @@
+<?php
+namespace IdeHelper\Generator\Task;
+
+use Cake\ORM\Association;
+use Cake\ORM\Association\BelongsTo;
+use Cake\ORM\Association\BelongsToMany;
+use Cake\ORM\Association\HasMany;
+use Cake\ORM\Association\HasOne;
+use Cake\ORM\Query;
+use Cake\ORM\Table;
+
+class TableAssociationTask extends ModelTask {
+
+	const CLASS_TABLE = Table::class;
+	const CLASS_ASSOCITATION = Association::class;
+	const CLASS_QUERY = Query::class;
+
+	/**
+	 * @var array
+	 */
+	protected $aliases = [
+		'\\' . self::CLASS_TABLE . '::belongsTo(0)' => BelongsTo::class,
+		'\\' . self::CLASS_TABLE . '::hasOne(0)' => HasOne::class,
+		'\\' . self::CLASS_TABLE . '::hasMany(0)' => HasMany::class,
+		'\\' . self::CLASS_TABLE . '::belongToMany(0)' => BelongsToMany::class,
+	];
+
+	/**
+	 * @return array
+	 */
+	public function collect() {
+		$models = $this->collectModels();
+
+		$result = [];
+		foreach ($this->aliases as $alias => $className) {
+			$map = [];
+			foreach ($models as $model => $modelClassName) {
+				$map[$model] = '\\' . $className . '::class';
+			}
+			$result[$alias] = $map;
+		}
+
+		return $result;
+	}
+
+}
