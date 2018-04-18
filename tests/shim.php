bd8ccdf9cbf21566c5a2eb9fc85b67607d83ff5d
mscherer <mark.scherer@spryker.com>
2018-04-18 17:18:39 +0200

    Run tests again for CakePHP 3.6.

--- tests/shim.php
+++ tests/shim.php
@@ -1,2 +1,9 @@
 <?php
+use PHP_CodeSniffer\Config;
+
+$manualAutoload = getcwd() . '/vendor/squizlabs/php_codesniffer/autoload.php';
+if (!class_exists(Config::class) && file_exists($manualAutoload)) {
+	require $manualAutoload;
+}
+
 error_reporting(E_ALL & ~E_USER_DEPRECATED);
