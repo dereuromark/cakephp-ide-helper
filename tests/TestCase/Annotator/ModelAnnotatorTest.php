80f5b9bb1ca300f2ed5036d68dc0a080c6499599
Walther Lalk <dakota@users.noreply.github.com>
2019-10-15 10:44:40 +0200

    Create a test to show that abstract parent classes break the model annotator

--- tests/TestCase/Annotator/ModelAnnotatorTest.php
+++ tests/TestCase/Annotator/ModelAnnotatorTest.php
@@ -244,4 +244,31 @@ class ModelAnnotatorTest extends TestCase {
 		return $this->getMockBuilder(ModelAnnotator::class)->setMethods(['_storeFile'])->setConstructorArgs([$this->io, $params])->getMock();
 	}
 
+	/**
+	 * @return void
+	 */
+	public function testAnnotateProtectedParent()
+	{
+		$annotator = $this->_getAnnotatorMock([]);
+
+		$expectedContent = str_replace("\r\n", "\n", file_get_contents(TEST_FILES . 'Model/Table/BarBarsAbstractTable.php'));
+		$callback = function ($value) use ($expectedContent) {
+			$value = str_replace(["\r\n", "\r"], "\n", $value);
+			if ($value !== $expectedContent) {
+				$this->_displayDiff($expectedContent, $value);
+			}
+
+			return $value === $expectedContent;
+		};
+		$annotator->expects($this->once())
+			->method('_storeFile')
+			->with($this->anything(), $this->callback($callback));
+
+		$path = APP . 'Model/Table/BarBarsAbstractTable.php';
+		$annotator->annotate($path);
+
+		$output = (string)$this->out->output();
+
+		$this->assertTextContains('  -> 14 annotations added', $output);
+	}
 }
