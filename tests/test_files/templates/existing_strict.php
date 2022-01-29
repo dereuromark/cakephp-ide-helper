4ae2dc8fa0ce6fe6ef1366146dcc9c0d4e822da3
mscherer <mark.scherer@spryker.com>
2022-01-29 01:17:52 +0100

    Test case for strict type issue

---
+++ tests/test_files/templates/existing_strict.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+/**
+ * @license MIT
+ */
+
+/**
+ * @var \TestApp\View\AppView $this
+ * @var \TestApp\Model\Entity\Wheel $wheel
+ */
+?>
+<div>
+	<?php echo h($wheel->id); ?>
+</div>
