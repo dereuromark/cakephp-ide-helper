cdd845f0311a7fe676ffdd4672d52a513fe58955
mscherer <mark.scherer@spryker.com>
2022-07-21 16:51:13 +0200

    Fix up template var detection.

--- tests/test_app/templates/Foos/existing.php
+++ tests/test_app/templates/Foos/existing.php
@@ -5,9 +5,14 @@
 
 /**
  * @var \TestApp\View\AppView $this
+ * @var array<int, int> $things
  */
 ?>
 <div>
 	<?php foreach ($cars as $car) {} ?>
 	<?php echo h($wheel->id); ?>
+
+	<?php foreach ($things as $keyInt => $valueInt) {
+		echo h($keyInt + $valueInt);
+	} ?>
 </div>
--- tests/test_files/templates/existing.php
+++ tests/test_files/templates/existing.php
@@ -5,6 +5,7 @@
 
 /**
  * @var \TestApp\View\AppView $this
+ * @var array<int, int> $things
  * @var \TestApp\Model\Entity\Car[]|\Cake\Collection\CollectionInterface $cars
  * @var \TestApp\Model\Entity\Wheel $wheel
  */
@@ -12,4 +13,8 @@
 <div>
 	<?php foreach ($cars as $car) {} ?>
 	<?php echo h($wheel->id); ?>
+
+	<?php foreach ($things as $keyInt => $valueInt) {
+		echo h($keyInt + $valueInt);
+	} ?>
 </div>
