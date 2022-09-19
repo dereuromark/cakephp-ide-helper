4a8bcacbfbcb5cc825b71736b81a5225ba923a88
mscherer <mark.scherer@spryker.com>
2022-09-19 11:52:35 +0200

    Fix up code completion for behaviors

--- src/CodeCompletion/Task/ModelEventsTask.php
+++ src/CodeCompletion/Task/ModelEventsTask.php
@@ -20,32 +20,40 @@ class ModelEventsTask implements TaskInterface {
 	 * @return string
 	 */
 	public function create(): string {
-		return <<<HERE
+		$events = <<<TXT
+	public function beforeMarshal(EventInterface \$event, ArrayObject \$data, ArrayObject \$options): void;
+	public function afterMarshal(EventInterface \$event, EntityInterface \$entity, ArrayObject \$data, ArrayObject \$options): void;
+	public function beforeFind(EventInterface \$event, Query \$query, ArrayObject \$options, \$primary): void;
+	public function buildValidator(EventInterface \$event, Validator \$validator, \$name);
+	public function buildRules(EventInterface \$event, RulesChecker \$rules);
+	public function beforeRules(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options, \$operation): void;
+	public function afterRules(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options, \$result, \$operation): void;
+	public function beforeSave(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options): void;
+	public function afterSave(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options): void;
+	public function afterSaveCommit(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options): void;
+	public function beforeDelete(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options): void;
+	public function afterDelete(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options): void;
+	public function afterDeleteCommit(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options): void;
+TXT;
 
-        use ArrayObject;
-        use Cake\Datasource\EntityInterface;
-        use Cake\Event\EventInterface;
-        use Cake\Validation\Validator;
+		return <<<TXT
 
-        class Table
-        {
-            public function beforeMarshal(EventInterface \$event, ArrayObject \$data, ArrayObject \$options);
-            public function afterMarshal(EventInterface \$event, EntityInterface \$entity, ArrayObject \$data, ArrayObject \$options);
-            public function beforeFind(EventInterface \$event, Query \$query, ArrayObject \$options, \$primary);
-            public function buildValidator(EventInterface \$event, Validator \$validator, \$name);
-            public function buildRules(EventInterface \$event, RulesChecker \$rules);
-            public function beforeRules(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options, \$operation);
-            public function afterRules(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options, \$result, \$operation);
-            public function beforeSave(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options);
-            public function afterSave(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options);
-            public function afterSaveCommit(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options);
-            public function beforeDelete(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options);
-            public function afterDelete(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options);
-            public function afterDeleteCommit(EventInterface \$event, EntityInterface \$entity, ArrayObject \$options);
-        }
+use ArrayObject;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\EventInterface;
+use Cake\Validation\Validator;
 
-        
-        HERE;
+class Table
+{
+$events
+}
+
+class Behavior
+{
+$events
+}
+
+TXT;
 	}
 
 }
