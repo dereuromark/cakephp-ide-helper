d9fc0d185170639ae35ed2d3ba21e549d02d4a7f
mscherer <mark.scherer@spryker.com>
2022-10-04 23:43:17 +0200

    Fix PHPStan

--- tests/shim.php
+++ tests/shim.php
@@ -12,6 +12,9 @@ error_reporting(E_ALL & ~E_USER_DEPRECATED);
 if (!defined('T_NULLABLE')) {
 	define('T_NULLABLE', 'PHPCS_T_NULLABLE');
 }
+if (!defined('T_DOC_COMMENT_TAG')) {
+	define('T_DOC_COMMENT_TAG', 'PHPCS_T_DOC_COMMENT_TAG');
+}
 
 if (!class_exists(\PHPUnit\Framework\TestCase::class)) {
 	require 'TestCase.php';
