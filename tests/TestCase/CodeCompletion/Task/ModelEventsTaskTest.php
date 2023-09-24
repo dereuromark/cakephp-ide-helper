05c8feda5809163c4d7cee08faf89fa3dab3844f
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2023-09-24 15:48:22 +0200

    Incorrect types for ModelEventsTask.php (#326)
    * Add missing typing to CodeCompletionGeneratorTest.php
    
    * Add missing typing to ModelEventsTaskTest.php
    
    * Add missing types for ModelEventsTask.php
    
    * Update signature according to PHP 5
    
    https://github.com/cakephp/cakephp/blob/5.0.0/src/ORM/Table.php#L141-L153
    
    * Sort `use` alphabetically
    
    * Wrong boolean type
    
    * Use SelectQuery from ORM

--- tests/TestCase/CodeCompletion/Task/ModelEventsTaskTest.php
+++ tests/TestCase/CodeCompletion/Task/ModelEventsTaskTest.php
@@ -29,17 +29,19 @@ class ModelEventsTaskTest extends TestCase {
 use ArrayObject;
 use Cake\Datasource\EntityInterface;
 use Cake\Event\EventInterface;
+use Cake\ORM\Query\SelectQuery;
+use Cake\ORM\RulesChecker;
 use Cake\Validation\Validator;
 
 if (false) {
 	class Table {
 		public function beforeMarshal(EventInterface $event, ArrayObject $data, ArrayObject $options): void {}
-		public function afterMarshal(EventInterface $event, EntityInterface $entity, ArrayObject $data, ArrayObject $options): void {}
-		public function beforeFind(EventInterface $event, Query $query, ArrayObject $options, $primary): void {}
-		public function buildValidator(EventInterface $event, Validator $validator, $name): void {}
+		public function afterMarshal(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
+		public function beforeFind(EventInterface $event, SelectQuery $query, ArrayObject $options, bool $primary): void {}
+		public function buildValidator(EventInterface $event, Validator $validator, string $name): void {}
 		public function buildRules(RulesChecker $rules): RulesChecker { return $rules; }
-		public function beforeRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, $operation): void {}
-		public function afterRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, $result, $operation): void {}
+		public function beforeRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, string $operation): void {}
+		public function afterRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, bool $result, string $operation): void {}
 		public function beforeSave(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
 		public function afterSave(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
 		public function afterSaveCommit(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
@@ -50,12 +52,12 @@ if (false) {
 
 	class Behavior {
 		public function beforeMarshal(EventInterface $event, ArrayObject $data, ArrayObject $options): void {}
-		public function afterMarshal(EventInterface $event, EntityInterface $entity, ArrayObject $data, ArrayObject $options): void {}
-		public function beforeFind(EventInterface $event, Query $query, ArrayObject $options, $primary): void {}
-		public function buildValidator(EventInterface $event, Validator $validator, $name): void {}
+		public function afterMarshal(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
+		public function beforeFind(EventInterface $event, SelectQuery $query, ArrayObject $options, bool $primary): void {}
+		public function buildValidator(EventInterface $event, Validator $validator, string $name): void {}
 		public function buildRules(RulesChecker $rules): RulesChecker { return $rules; }
-		public function beforeRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, $operation): void {}
-		public function afterRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, $result, $operation): void {}
+		public function beforeRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, string $operation): void {}
+		public function afterRules(EventInterface $event, EntityInterface $entity, ArrayObject $options, bool $result, string $operation): void {}
 		public function beforeSave(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
 		public function afterSave(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
 		public function afterSaveCommit(EventInterface $event, EntityInterface $entity, ArrayObject $options): void {}
