77c6bb8ce830bdce4ff4f09891f3244e33ca2e7f
mscherer <dereuromark@web.de>
2024-02-14 13:56:22 +0100

    Test that all different array annotations work.

---
+++ tests/test_files/templates/array.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @var \TestApp\View\AppView $this
+ * @var array $x
+ * @var array<int> $ints
+ * @var array{a: int, b: string}|null $shaped
+ * @var mixed $foo
+ */
+	foreach ($x as $y) {
+		echo $y;
+	}
+	foreach ($foo as $int) {
+		echo $int;
+	}
+?>
+<div>
+	<?php foreach ($ints as $int) {
+		echo $int;
+	} ?>
+	<?php foreach ($shaped as $x) {
+		echo h($x);
+	} ?>
+</div>
