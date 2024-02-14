e425560dbcb322adf27e82fab525e5461b21b015
mscherer <dereuromark@web.de>
2024-02-14 14:46:56 +0100

    Duplicate removal testing.

--- tests/TestCase/Illuminator/IlluminatorTest.php
+++ tests/TestCase/Illuminator/IlluminatorTest.php
@@ -43,7 +43,7 @@ class IlluminatorTest extends TestCase {
 		$path = TEST_FILES;
 		$count = $this->illuminator->illuminate($path, null);
 
-		$this->assertSame(10, $count);
+		$this->assertSame(11, $count);
 
 		$out = $this->out->output();
 
