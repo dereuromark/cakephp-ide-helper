b7da2b77e9b176437dcde89c1cb0f1a8412f6982
Mark Scherer <dereuromark@users.noreply.github.com>
2025-07-22 14:30:14 +0200

    Adjust to skip URLs in link annotations. (#392)

--- tests/test_files/View/AppView.php
+++ tests/test_files/View/AppView.php
@@ -4,6 +4,7 @@ namespace TestApp\View;
 use Cake\View\View;
 
 /**
+ * @link https://book.cakephp.org/5/en/views.html#the-app-view
  * @property \TestApp\View\Helper\HtmlHelper $Html
  * @property \TestApp\View\Helper\MyHelper $My
  * @property \Shim\View\Helper\ConfigureHelper $Configure
