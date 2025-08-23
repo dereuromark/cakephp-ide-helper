b1303fde8a26f86e8899c573a25c2b3fb9b739bb
Mark <dereuromark@users.noreply.github.com>
2025-08-23 10:06:05 +0000

    Auto-fix CS - CS master branch

--- config/app.example.php
+++ config/app.example.php
@@ -4,8 +4,7 @@ return [
 	 // Copy the following over to your project one in ROOT/config/
 	'IdeHelper' => [
 		// Additional plugins that are not loaded, but should be included, use `-` prefix to exclude
-		'plugins' => [
-		],
+		'plugins' => [],
 		// Controller prefixes to check for
 		'prefixes' => [
 			'Admin',
@@ -29,36 +28,29 @@ return [
 		// Set to `false` to disable, defaults to `mixed` if enabled, you can also pass callable for logic
 		'autoCollect' => true,
 		// Can be strings or `/regex/` (e.g. `'/^\_.+$/i'` for underscore prefixed variables)
-		'autoCollectBlacklist' => [
-		],
+		'autoCollectBlacklist' => [],
 		'preferLinkOverUsesInTests' => false, // Prefer `@link` annotations over `@uses` in test files, prevents PHPUnit to replace with annotations
 		// Custom Entity field type mapping
-		'typeMap' => [
-		],
+		'typeMap' => [],
 		// Default View class to use
 		'viewClass' => null,
 		// Plugins to include for View annotations
-		'includedPlugins' => [
-		],
+		'includedPlugins' => [],
 		// Always add annotations/meta even if not yet needed
 		'preemptive' => false,
 		// Annotator task customization
-		'annotators' => [
-		],
+		'annotators' => [],
 		// For meta file generator
-		'generatorTasks' => [
-		],
+		'generatorTasks' => [],
 		// A regex pattern - for Migrations plugin DatabaseTableTask generator task
 		'ignoreDatabaseTables' => null,
 		// A list of tables - for Migrations plugin DatabaseTableTask generator task
 		'skipDatabaseTables' => null,
 		// For Illuminator tasks
-		'illuminatorTasks' => [
-		],
+		'illuminatorTasks' => [],
 		'illuminatorIndentation' => "\t",
 		// For code completion file generator
-		'codeCompletionTasks' => [
-		],
+		'codeCompletionTasks' => [],
 		// If a custom directory should be used, defaults to TMP otherwise
 		'codeCompletionPath' => null,
 		'codeCompletionReturnType' => null, // Auto-detect based on controller/component, set to true/false to force one mode.
